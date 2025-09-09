0e06fe31f20a63149811c5e03502217fcc3ccf58
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-09 17:23:29 +0600

    added modal functionality

--- script.js
+++ script.js
@@ -56,6 +56,21 @@ const bookmarked = (article) => {
     bookmarkContainer.appendChild(div);
 };
 
+const modal = (article) => {
+    const modalBox = document.getElementById("show-modal");
+    const modalEl = document.getElementById("my_modal_2");
+
+    modalBox.innerHTML = `
+        <div class="border border-gray-300 rounded-lg p-5">
+            <img class="w-full object-cover" src="${article.image.srcset[5].url}" />
+            <h1 class="font-bold text-base md:text-lg mt-3">${article.title}</h1>
+            <p class="opacity-80 text-sm md:text-base">${article.time}</p>
+        </div>
+    `;
+    modalEl.showModal();
+};
+
+
 
 
 const showNewsByCategory = (articles) =>{
@@ -66,7 +81,7 @@ const showNewsByCategory = (articles) =>{
             <div>
                 <img src="${article.image.srcset[5].url}"
             </div>
-            <h1 class="font-bold text-base md:text-lg">${article.title}</h1>
+            <h1 onclick='modal(${JSON.stringify(article)})' class="font-bold text-base md:text-lg">${article.title}</h1>
             <div class="flex justify-between items-center">
                 <p class="opacity-80 text-sm md:text-base">${article.time}</p>
                 <i onclick='bookmarked(${JSON.stringify(article)})' class="fa-solid fa-bookmark" style="color: #B91C1C;"></i>
